224863771ef4eb887115dac5d61d3adceed6f244
Ramon Arguello <ramonarguello@monzo.com>
2021-09-24 15:52:58 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -124,7 +124,7 @@ let package = Package(
             targets: ["Realm", "RealmSwift"]),
     ],
     dependencies: [
-        .package(name: "RealmDatabase", url: "https://github.com/monchote/realm-core", .branch("master"))
+        .package(url: "https://github.com/monchote/realm-core", .branch("master"))
     ],
     targets: [
       .target(
